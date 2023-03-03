56da4bfac99744d1f4e5ff360f5042322474ebcb
hanggarahadianto <93454559+hanggarahadianto@users.noreply.github.com>
2023-03-03 15:47:59 +0700

    second commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const util = require("util");
 const fs = require("fs");
-const Excel = require('exceljs');
+const Excel = require("exceljs");
 const path = require("path");
 const uuid = require("uuid");
 
@@ -20,77 +20,75 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const questionnaire = [
-  {
-    // date_created: "2023-03-01T07:01:44.000Z",
-    // date_updated: "2023-03-01T07:01:44.000Z",
-    // id: 98,
-    // sort: null,
-    doc_no: "FM-HSE-11",
-    nama_form: 6,
-    nama_cabang: null,
-    tanggal_dibuat: "2023-03-01T07:01:44",
-    tanggal_revisi: null,
-    user: "b452df24-a638-44a0-8cbf-a71647f28234",
-    user_created: "16c150fe-3d8b-42d3-81d7-9fc2cc227761",
-    user_updated: "16c150fe-3d8b-42d3-81d7-9fc2cc227761",
-    answer_details: [
-      {
-        question_id: 5,
-        nilai: 10,
-        keterangan: "bagus",
-      },
-    ],
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("Questioner", null, null, {
+  username: "root",
+  password: "12345678",
+  database: "Questioner",
+  host: "localhost",
+  dialect: "mysql",
+  logging: false,
+  dialectOptions: {
+    options: {
+      requestTimeout: 1000000,
+      connectTimeout: 1000000,
+    },
   },
-];
+});
+const { QueryTypes } = require("sequelize");
 
-// const convertJsonToExcel = () => {
-//   const workSheet = XLSX.utils.json_to_sheet(questionnaire);
-//   const workBook = XLSX.utils.book_new();
+const tes = async () => {
+  const submission_details = await sequelize.query("SELECT * FROM submission WHERE doc_no = 'FM-HSE-13'", {
+    type: QueryTypes.SELECT,
+  });
 
-//   XLSX.utils.book_append_sheet(workBook, workSheet, "questionnaire");
-//   // Generate buffer
-//   XLSX.write(workBook, { bookType: "xlsx", type: "buffer" });
+  const answer_details = await sequelize.query("SELECT * FROM answer WHERE id = 56", {
+    type: QueryTypes.SELECT,
+  });
 
-//   // Binary string
-//   XLSX.write(workBook, { bookType: "xlsx", type: "binary" });
+  const question_details = await sequelize.query("SELECT * FROM question WHERE id = 16", {
+    type: QueryTypes.SELECT,
+  });
 
-//   XLSX.writeFile(workBook, "questionnaireData.xlsx");
-// };
-// convertJsonToExcel();
+  const form_details = await sequelize.query("SELECT * FROM form WHERE id = 6", {
+    type: QueryTypes.SELECT,
+  });
 
-const tes = () => {
   const workbook = new Excel.Workbook();
+
   const name = `new-${uuid.v4()}.xlsx`;
-    workbook.xlsx.readFile('old.xlsx')
-    .then(function() {
-        var worksheet = workbook.getWorksheet(1);
-        var row = worksheet.getRow(1);
-        row.getCell(4).value = "Tes Judul Excel"; 
-        row.getCell(8).value = "no/22/001"; 
-        row.commit();
+  workbook.xlsx.readFile("old.xlsx").then(function () {
+    var worksheet = workbook.getWorksheet(1);
 
-        var row2 = worksheet.getRow(2);
-        row2.getCell(8).value = "1 maret 2023"; 
-        row2.commit();
+    var row = worksheet.getRow(1);
+    row.getCell(4).value = form_details[0].nama_form;
+    row.getCell(8).value = submission_details[0].doc_no;
+    row.commit();
 
+    var row2 = worksheet.getRow(2);
+    row2.getCell(8).value = submission_details[0].tanggal_dibuat;
+    row2.commit();
 
-        var row7 = worksheet.getRow(7);
-        row7.getCell(1).value = "1.1"; 
-        row7.getCell(3).value = "Bahan dan alat tertata rapi"; 
-        row7.getCell(4).value = "F"; 
-        row7.getCell(8).value = "10"; 
-        
-        row7.commit();
-        return workbook.xlsx.writeFile(name);
-    })
-  };
-  tes();
+    var row3 = worksheet.getRow(3);
+    row3.getCell(8).value = submission_details[0].tanggal_revisi;
+    row3.commit();
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "convert" });
-});
+    var row4 = worksheet.getRow(4);
+    row4.getCell(8).value = submission_details[0].nama_cabang;
+    row4.commit();
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+    var row7 = worksheet.getRow(7);
+    row7.getCell(1).value = "1.1";
+    row7.getCell(2).value = question_details[0].indikator;
+    row7.getCell(4).value = question_details[0].metode_verifikasi;
+    row7.getCell(5).value = answer_details[0].metode_verifikasi;
+    row7.getCell(7).value = answer_details[0].nilai;
+    row7.getCell(8).value = question_details[0].bobot;
+    row7.getCell(9).value = answer_details[0].keterangan;
+    row7.commit();
+
+    return workbook.xlsx.writeFile(name);
+  });
+};
+tes();
